b2af86d6254a14641ba989b207fdfddecbbcf64c
Lucas-290321 <lucaraymundopinheiro@gmail.com>
2026-05-04 22:38:45 -0300

    Atualizar logo do HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -36,7 +36,7 @@ export default function HomePage() {
               >
                 <div className="mb-8">
                   <img
-                    src="https://horizons-cdn.hostinger.com/8d4d2314-a9fa-472c-8ad8-c9a7c6159296/732aadc38a3e27cb7550eb781fe5ee00.jpg"
+                    src="https://cdn.discordapp.com/attachments/1486159287070756995/1501032239172030585/WhatsApp_Image_2026-05-03_at_11.08.08.jpeg?ex=69fa98b5&is=69f94735&hm=ae77458b9be21454e241ba548a84988c1e3afc850d9e7996bd26a0fc71b2f476&"
                     alt="Axyon Logo"
                     className="h-20 w-20 rounded-2xl mb-6"
                   />
